28fdb09d2428e743a599a31f437a1415b08f019b
Leonardo Yukdawan Liwan <35754346+leoliwan@users.noreply.github.com>
2019-04-11 18:25:33 +0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from "../components/layout"
-import { Container, Col, Row Card } from 'react-bootstrap'
+import { Container, Col, Row, Card } from 'react-bootstrap'
 
 const Contact = () => (
     <Layout>
